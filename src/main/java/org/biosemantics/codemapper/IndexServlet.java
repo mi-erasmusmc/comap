5fd287f8c32f5eff66ada2f2e4b3db90803cdc89
Benedikt Benus Becker <b.becker@erasmusmc.nl>
2016-03-12 16:17:11 +0100

    Serve index as JSP servlet

---
+++ src/main/java/org/biosemantics/codemapper/IndexServlet.java
@@ -0,0 +1,25 @@
+package org.biosemantics.codemapper;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("")
+public class IndexServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public void doGet (HttpServletRequest req,
+                       HttpServletResponse res)
+      throws ServletException, IOException
+    {
+        req.setCharacterEncoding("UTF-8");
+        getServletContext()
+            .getRequestDispatcher("/WEB-INF/index.jsp")
+            .forward(req, res);
+    }
+}

8744e6e90e8ae3253779e3ab91a041977774b570
Benedikt Becker <benedikt.becker@inria.fr>
2019-12-13 12:30:44 +0100

    Reset password in user change form

--- codemapper_manager/manage_codemapper/models.py
+++ codemapper_manager/manage_codemapper/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
 
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=64)
-    email = models.TextField()
+    email = models.CharField(max_length=120)
 
     def __str__(self):
         return self.username
